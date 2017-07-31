f13e1e028b76ca9a0642545ef5ea5cc5e29b2e9b
Gabriel <squad_gl@hotmail.com>
2017-07-31 19:02:44 -0400

    Initial commit

---
+++ app/src/main/java/cl/mind/shrinkquizz/SectionsPagerAdapter.java
@@ -0,0 +1,51 @@
+package cl.mind.shrinkquizz;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+
+    //        constructor
+    public SectionsPagerAdapter(FragmentManager fm) {
+
+        super(fm);
+    }
+
+//        fin contructor
+
+    @Override
+    public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    return PartyFragment.newInstance();
+                case 1:
+                    return MatchFragment.newInstance();
+                case 2:
+                    return LuckyFragment.newInstance();
+                default:
+                    return PartyFragment.newInstance();
+            }
+   }
+
+    @Override
+    public int getCount() {
+        // Show 3 total pages.
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Party";
+            case 1:
+                return "Match";
+            case 2:
+                return "Lucky";
+        }
+        return null;
+    }
+}
